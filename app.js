23180a904041d13db687e4cb702120299af7af08
Matthew Roseman <mroseman95@gmail.com>
2018-03-02 18:44:01 -0500

    broke out sending messages into it's own function
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ var ChordNode = function(hostname, port) {
         'port': this.port,
         'id': this.id
     };
+    this.fingers = [];
+    this.successor = null;
+    this.predecessor = null;
 
     // ith entry in this table contains id of first node, s, that succeeds this node by
     // at least  2^(i-1)
@@ -53,36 +56,54 @@ var ChordNode = function(hostname, port) {
     }
 
     /*
+    sendMessage will send some message to the specified node, and return Promise with the response
+    */
+    function sendMessage(node, msg) {
+        var client = new net.Socket();
+        client.setTimeout(1000);
+
+        return new Promise((resolve, reject) => {
+            client.connect(node.port, node.hostname, () => {
+                client.on('data', (data) => {
+                    data = JSON.parse(data);
+                    // TODO check that id in data matches node.id
+                    id = data.id;
+                    msg = data.msg;
+
+                    return resolve(msg);
+                });
+
+                client.on('timeout', () => {
+                    reject(Error('request timeout'));
+                });
+
+                client.on('error', (err) => {
+                    reject(err);
+                });
+
+                client.write(JSON.stringify({'id': this.id, 'msg': msg}));
+            });
+        });
+    }
+
+    /*
     pingNode will accept a node object, and ping it, and check if there is a pong response
     @return: a Promise that will be true on successful ping and pong if there is no response
     */
     this.pingNode = function(node) {
         return new Promise((resolve, reject) => {
-            var client = new net.Socket();
-            client.setTimeout(1000);
-
-            client.connect(node.port, node.hostname, function() {
-                client.on('data', (data) => {
-                    data = JSON.parse(data);
-                    id = data.id;
-                    msg = data.msg;
-
-                    // TODO check id?
-                    if (msg === 'pong') {
+            sendMessage(node, 'ping')
+                .then((response) => {
+                    if (response === 'pong') {
                         resolve('success');
                     }
+
+                    reject('unkown response');
+                }).catch((err) => {
+                    if (err.message === 'request timeout') {
+                        reject(Error('ping timeout'));
+                    }
                 });
-
-                client.on('timeout', () => {
-                    reject(Error('Ping timeout'));
-                });
-
-                client.write(JSON.stringify({'id': 1, 'msg': 'ping'}));
-            });
-
-            client.on('error', (err) => {
-                reject(err);
-            });
         });
     };
 };
--- test/test_chord_node.js
+++ test/test_chord_node.js
@@ -13,6 +13,18 @@ describe('ChordNode', function() {
         node.id.should.be.exactly('98c4c77306bfa6e5a8e574070441b676050f3166');
     });
 
+    it('should be created with an empty finger table', function() {
+        node.fingers.should.be.empty;
+    });
+
+    it('should be created with a null successor', function() {
+        (node.successor === null).should.be.true;
+    });
+
+    it('should be created with a null predecessor', function() {
+        (node.predecessor === null).should.be.true;
+    });
+
     it('should respond to \'ping\' message with a \'pong\' message', function(done) {
         var client = new net.Socket();
         client.connect(41234, '127.0.0.1', function() {
@@ -45,10 +57,10 @@ describe('ChordNode', function() {
         });
         testServer.listen(41236, '127.0.0.1');
 
-        it ('should return a rejected promise if the given node doesn\'t exist', function() {
+        it ('should return a rejected promise if the given node timesout', function() {
             this.timeout(3000);
             return node.pingNode({'id': 1, 'hostname': '127.0.0.1', 'port': 41236})
-                .should.be.rejectedWith('Ping timeout')
+                .should.be.rejectedWith('ping timeout')
         });
 
         testServer.close();
